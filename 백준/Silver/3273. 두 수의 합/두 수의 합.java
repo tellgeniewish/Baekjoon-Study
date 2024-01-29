afdc00599e59385f29850717775870e1a739aa33
tellgeniewish <147725266+tellgeniewish@users.noreply.github.com>
2024-01-30 06:23:00 +0900

    [Silver III] Title: 두 수의 합, Time: 1700 ms, Memory: 108524 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,35 @@
+// tell genie wish 1236 CH03.배열
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+        Arrays.sort(arr);
+		
+		int x = sc.nextInt();
+		
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] + arr[j] == x)
+					count++;
+				if (x < arr[i] + arr[j])
+					break;
+			}
+		}
+		
+		System.out.println(count);
+		
+		sc.close();
+	}
+
+}
